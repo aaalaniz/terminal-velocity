457113388c3e14c232f0da6bb0f2914e4ee7e671
Aaron Alaniz <aaron@aalaniz.com>
2025-12-30 19:25:20 -0600

    Add column with placeholder options in title screen

--- src/main/java/xyz/alaniz/aaron/ui/title/TitleScreenUi.kt
+++ src/main/java/xyz/alaniz/aaron/ui/title/TitleScreenUi.kt
@@ -1,13 +1,23 @@
 package xyz.alaniz.aaron.ui.title
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import com.jakewharton.mosaic.layout.height
+import com.jakewharton.mosaic.modifier.Modifier
+import com.jakewharton.mosaic.ui.Column
+import com.jakewharton.mosaic.ui.Spacer
 import com.jakewharton.mosaic.ui.Text
+import com.jakewharton.mosaic.ui.UnderlineStyle
 import com.slack.circuit.codegen.annotations.CircuitInject
 import dev.zacsweers.metro.AppScope
 
 @Composable
 @CircuitInject(screen = TitleScreen::class, scope = AppScope::class)
-fun TitleScreenUi(titleScreenState: TitleScreenState, modifier: Modifier) {
-    Text(value = "Title Screen")
+fun TitleScreenUi(titleScreenState: TitleScreenState, modifier: androidx.compose.ui.Modifier) {
+    val modifier = Modifier
+    Column {
+        Text(value = "Terminal Velocity")
+        Spacer(modifier.height(2))
+        Text(value = "Start game", underlineStyle = UnderlineStyle.Straight)
+        Text(value = "Options")
+    }
 }
