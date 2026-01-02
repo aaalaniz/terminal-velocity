1e117d7b03373dd9e9fb3d9f233453ee3d158def
Aaron Alaniz <aaron@aalaniz.com>
2026-01-01 19:46:20 -0600

    Setup key navigation

--- src/main/java/xyz/alaniz/aaron/ui/title/TitleScreenUi.kt
+++ src/main/java/xyz/alaniz/aaron/ui/title/TitleScreenUi.kt
@@ -1,7 +1,9 @@
 package xyz.alaniz.aaron.ui.title
 
 import androidx.compose.runtime.Composable
+import com.jakewharton.mosaic.layout.KeyEvent
 import com.jakewharton.mosaic.layout.height
+import com.jakewharton.mosaic.layout.onKeyEvent
 import com.jakewharton.mosaic.modifier.Modifier
 import com.jakewharton.mosaic.ui.Column
 import com.jakewharton.mosaic.ui.Spacer
@@ -10,11 +12,22 @@ import com.jakewharton.mosaic.ui.UnderlineStyle
 import com.slack.circuit.codegen.annotations.CircuitInject
 import dev.zacsweers.metro.AppScope
 
+private val Enter = KeyEvent("Enter")
+
+private val ArrowUp = KeyEvent("ArrowUp")
+private val k = KeyEvent("k")
+private val K = KeyEvent("K")
+
+private val ArrowDown = KeyEvent("ArrowDown")
+private val j = KeyEvent("j")
+private val J = KeyEvent("J")
+
 @Composable
 @CircuitInject(screen = TitleScreen::class, scope = AppScope::class)
 fun TitleScreenUi(titleScreenState: TitleScreenState, modifier: androidx.compose.ui.Modifier) {
     val modifier = Modifier
-    Column {
+
+    Column(modifier = modifier.onTitleScreenKeyEvent(titleScreenState)) {
         Text(value = "Terminal Velocity")
         Spacer(modifier.height(2))
 
@@ -26,6 +39,22 @@ fun TitleScreenUi(titleScreenState: TitleScreenState, modifier: androidx.compose
             }
 
             Text(value = option.label, underlineStyle = underlineStyle)
+        }
+    }
+}
+
+private fun Modifier.onTitleScreenKeyEvent(titleScreenState: TitleScreenState): Modifier {
+    return this.onKeyEvent { keyEvent ->
+        when (keyEvent) {
+            ArrowDown, J, j -> {
+                titleScreenState.onEvent(TitleScreenEvent.NextTitleOption)
+                return@onKeyEvent true
+            }
+            ArrowUp, K, k -> {
+                titleScreenState.onEvent(TitleScreenEvent.PreviousTitleOption)
+                return@onKeyEvent true
+            }
+            else -> return@onKeyEvent false
         }
     }
 }
