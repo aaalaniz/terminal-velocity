24445e458a84388a43c84797ddd3659fd2cf431d
Aaron Alaniz <aaron@aalaniz.com>
2026-01-03 15:08:16 -0600

    feat(game): Wire up keyboard input in GameUi

--- src/main/java/xyz/alaniz/aaron/ui/game/GameScreenUi.kt
+++ src/main/java/xyz/alaniz/aaron/ui/game/GameScreenUi.kt
@@ -1,18 +1,45 @@
 package xyz.alaniz.aaron.ui.game
 
 import androidx.compose.runtime.Composable
+import com.jakewharton.mosaic.layout.onKeyEvent
+import com.jakewharton.mosaic.modifier.Modifier
 import com.jakewharton.mosaic.ui.Color
 import com.jakewharton.mosaic.ui.Column
 import com.jakewharton.mosaic.ui.Row
 import com.jakewharton.mosaic.ui.Text
 import com.slack.circuit.codegen.annotations.CircuitInject
 import dev.zacsweers.metro.AppScope
+import xyz.alaniz.aaron.ui.foundation.KeyEvents.Enter
 
 @Composable
 @CircuitInject(screen = GameScreen::class, scope = AppScope::class)
 fun GameScreenUi(state: GameState, modifier: androidx.compose.ui.Modifier) {
     if (state is GameState.State) {
-        Column {
+        Column(modifier = Modifier.onKeyEvent { keyEvent ->
+            when (keyEvent) {
+                Enter -> {
+                    if (state.status == GameStatus.WAITING) {
+                        state.eventSink(GameEvent.GameStarted)
+                    } else if (state.status == GameStatus.GAME_OVER) {
+                        state.eventSink(GameEvent.GameReset)
+                    }
+                    true
+                }
+                else -> {
+                    if (state.status == GameStatus.PLAYING) {
+                        val key = keyEvent.key
+                        if (key != null && key.length == 1) {
+                            state.eventSink(GameEvent.LetterTyped(key[0]))
+                            true
+                        } else {
+                            false
+                        }
+                    } else {
+                        false
+                    }
+                }
+            }
+        }) {
             Text("Score: ${state.score}")
             Text("")
 
