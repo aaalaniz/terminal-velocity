d3721435985a45e452f867144fe4e9ec9b774387
Aaron Alaniz <aaron@aalaniz.com>
2026-01-03 15:07:43 -0600

    feat(game): Implement GameUi with Mosaic

--- src/main/java/xyz/alaniz/aaron/ui/game/GameScreenUi.kt
+++ src/main/java/xyz/alaniz/aaron/ui/game/GameScreenUi.kt
@@ -1,6 +1,9 @@
 package xyz.alaniz.aaron.ui.game
 
 import androidx.compose.runtime.Composable
+import com.jakewharton.mosaic.ui.Color
+import com.jakewharton.mosaic.ui.Column
+import com.jakewharton.mosaic.ui.Row
 import com.jakewharton.mosaic.ui.Text
 import com.slack.circuit.codegen.annotations.CircuitInject
 import dev.zacsweers.metro.AppScope
@@ -8,5 +11,27 @@ import dev.zacsweers.metro.AppScope
 @Composable
 @CircuitInject(screen = GameScreen::class, scope = AppScope::class)
 fun GameScreenUi(state: GameState, modifier: androidx.compose.ui.Modifier) {
-    Text("Game screen")
+    if (state is GameState.State) {
+        Column {
+            Text("Score: ${state.score}")
+            Text("")
+
+            when (state.status) {
+                GameStatus.WAITING -> {
+                    Text("Press 'Enter' to Start")
+                }
+                GameStatus.PLAYING -> {
+                    Row {
+                        Text(state.userInput, color = Color.Green)
+                        Text(state.currentWord.drop(state.userInput.length))
+                    }
+                }
+                GameStatus.GAME_OVER -> {
+                    Text("GAME OVER!", color = Color.Red)
+                    Text("Score: ${state.score}")
+                    Text("Press 'Enter' to Reset")
+                }
+            }
+        }
+    }
 }
