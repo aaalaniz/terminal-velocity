812adc95c7f4ce296a80a53fa1705f2b6f8177c2
Aaron Alaniz <aaron@aalaniz.com>
2026-01-03 14:27:48 -0600

    feat(game): Create GameEvent and GameState contract

---
+++ src/main/java/xyz/alaniz/aaron/ui/game/GameContract.kt
@@ -0,0 +1,26 @@
+package xyz.alaniz.aaron.ui.game
+
+import com.slack.circuit.runtime.CircuitUiEvent
+import com.slack.circuit.runtime.CircuitUiState
+
+sealed interface GameState : CircuitUiState {
+    data class State(
+        val currentWord: String,
+        val userInput: String,
+        val score: Int,
+        val status: GameStatus,
+        val eventSink: (GameEvent) -> Unit
+    ) : GameState
+}
+
+enum class GameStatus {
+    WAITING,
+    PLAYING,
+    GAME_OVER
+}
+
+sealed interface GameEvent : CircuitUiEvent {
+    data class LetterTyped(val char: Char) : GameEvent
+    data object GameStarted : GameEvent
+    data object GameReset : GameEvent
+}
