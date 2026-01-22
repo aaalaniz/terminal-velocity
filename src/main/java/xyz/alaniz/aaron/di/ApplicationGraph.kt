74fb30b702e02a842353116b80c484b09bc2c84d
Aaron Alaniz <aaron@aalaniz.com>
2026-01-22 05:46:33 -0600

    Remove changes to graph

--- src/main/java/xyz/alaniz/aaron/di/ApplicationGraph.kt
+++ src/main/java/xyz/alaniz/aaron/di/ApplicationGraph.kt
@@ -1,6 +1,5 @@
 package xyz.alaniz.aaron.di
 
-import com.slack.circuit.foundation.Circuit
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.DependencyGraph
 import dev.zacsweers.metro.Provides
@@ -8,7 +7,6 @@ import kotlinx.coroutines.CoroutineScope
 
 @DependencyGraph(AppScope::class)
 interface ApplicationGraph {
-  val circuit: Circuit
 
   @DependencyGraph.Factory
   fun interface Factory {
